f642d1473eff70c2211858acba64c16666787a36
NCr4wl3r <cr4wl3r@gmail.com>
2022-02-16 14:53:47 +0100

    reforge

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const PIEDRA = "PIEDRA";
 const TIJERA = "TIJERA";
 const PAPEL = "PAPEL";
 
+const RESULT_DRAW = "EMPATADO";
+const RESULT_WINNER = "GANADO";
+const RESULT_LOSER = "PERDIDO";
+
+let game_running = false;
+
 const cpuSelect = function () {
   const randomNum = Math.random();
 
@@ -16,21 +22,57 @@ const cpuSelect = function () {
   }
 };
 
-const userSelect = function () {
+const checkInputIsNull = function (uInput) {
+  if (uInput === null) return true;
+};
+
+const playerSelect = function () {
   const choice = prompt(
     `Escoge entre ${PIEDRA}, ${TIJERA} o ${PAPEL}!`
   ).toUpperCase();
+
   if (choice !== PIEDRA && choice !== TIJERA && choice !== PAPEL) {
     alert("Seleccion incorrecta");
-    return PIEDRA;
+    return playerSelect();
   }
   return choice;
 };
 
+const printResult = (cSelect, pSelect, result) => {
+  alert(
+    `La IA escogió ${cSelect}, y tu has escogido ${pSelect}. con lo cual has ${result}!!`
+  );
+  return;
+};
+
+const getWinner = function (cSelect, pSelect) {
+  let result;
+  if (cSelect === pSelect) {
+    result = RESULT_DRAW;
+  } else if (
+    (cSelect === PIEDRA && pSelect === TIJERA) ||
+    (cSelect === PAPEL && pSelect === PIEDRA) ||
+    (cSelect === TIJERA && pSelect === PAPEL)
+  ) {
+    result = RESULT_LOSER;
+  } else {
+    result = RESULT_WINNER;
+  }
+  return result;
+};
+
 const startGame = function () {
-  console.log("Game starting........");
-  console.log(cpuSelect());
-  console.log(userSelect());
+  if (!game_running) {
+    console.log("Game starting........");
+    game_running = true;
+
+    const cpuChoice = cpuSelect();
+    const playerChoice = playerSelect();
+    const winner = getWinner(cpuChoice, playerChoice);
+
+    printResult(cpuChoice, playerChoice, winner);
+    game_running = false;
+  }
 };
 
 startButton.addEventListener("click", startGame);
